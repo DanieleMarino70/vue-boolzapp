dc822e5caf4f2e51effddcd730f493fe82948d1d
DanieleMarino70 <danielaotitan@gmail.com>
2023-02-15 16:58:17 +0100

    add: Milestone 2

--- js/main.js
+++ js/main.js
@@ -5,11 +5,20 @@
 // // nome e immagine di ogni contatto
 
 // TODO Milestone 2
-// ** ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, 
+// ** ● Visualizzazione dinamica dei messaggi: 
+// ** tramite la direttiva v-for, 
 // ** visualizzare tutti i messaggi relativi al contatto attivo all’interno 
 // ** del pannello della conversazione
 
 // ** ● Click sul contatto mostra la conversazione del contatto cliccato
+
+// Milestone 3
+// ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+// “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+
+// ● Risposta dall’interlocutore: 
+//ad ogni inserimento di un messaggio, l’utente riceverà
+// un “ok” come risposta, che apparirà dopo 1 secondo.
 
 const app = Vue.createApp({
   data() {
@@ -177,12 +186,20 @@ const app = Vue.createApp({
           ],
         },
       ],
+      activeUser: -1,
     };
   },
 
-  methods: {},
+  methods: {
+    userSelected(index){
+      this.activeUser = index;
+    }
+  },
 
   created() {},
 });
 
 app.mount("#root");
+
+
+//(activeUser >= 0 ? `` : `${$contacts[activeUser].name}`)
