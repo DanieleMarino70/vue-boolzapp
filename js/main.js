d547c7d0c408b1c2238de5ac5e64db2b9067a388
DanieleMarino70 <danielaotitan@gmail.com>
2023-02-15 17:52:31 +0100

    add: Milestone 3

--- js/main.js
+++ js/main.js
@@ -4,21 +4,21 @@
 // // ● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
 // // nome e immagine di ogni contatto
 
-// TODO Milestone 2
-// ** ● Visualizzazione dinamica dei messaggi: 
-// ** tramite la direttiva v-for, 
-// ** visualizzare tutti i messaggi relativi al contatto attivo all’interno 
-// ** del pannello della conversazione
+// // ** Milestone 2
+// // ** ● Visualizzazione dinamica dei messaggi: 
+// // ** tramite la direttiva v-for, 
+// // ** visualizzare tutti i messaggi relativi al contatto attivo all’interno 
+// // ** del pannello della conversazione
 
-// ** ● Click sul contatto mostra la conversazione del contatto cliccato
+// // ** ● Click sul contatto mostra la conversazione del contatto cliccato
 
-// Milestone 3
-// ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
-// “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// TODO Milestone 3
+// ** ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+// ** “enter” il testo viene aggiunto al thread sopra, come messaggio verde
 
-// ● Risposta dall’interlocutore: 
-//ad ogni inserimento di un messaggio, l’utente riceverà
-// un “ok” come risposta, che apparirà dopo 1 secondo.
+// ** ● Risposta dall’interlocutore: 
+// ** ad ogni inserimento di un messaggio, l’utente riceverà
+// ** un “ok” come risposta, che apparirà dopo 1 secondo.
 
 const app = Vue.createApp({
   data() {
@@ -187,16 +187,43 @@ const app = Vue.createApp({
         },
       ],
       activeUser: -1,
+      indexUserResponse: -1,
+      newMessage: "",
     };
   },
 
   methods: {
     userSelected(index){
       this.activeUser = index;
-    }
+    },
+    addMessage(input){
+      const messageToSend = {
+        date: "12:00",
+        message: input,
+        status: "sent" 
+      };
+      this.contacts[this.activeUser].messages.push(messageToSend);
+      this.indexUserResponse = this.activeUser;
+      this.newMessage="";
+    },
+
+     userResponse(){
+        setTimeout(() =>{
+          const messageToReceive = {
+            date: "12:00",
+            message: "Ok",
+            status: "received"
+          };
+          this.contacts[this.indexUserResponse].messages.push(messageToReceive);
+        }, 1000)
+     }
   },
 
-  created() {},
+  created() {
+    // setTimeout(()=>{
+    //   this.userRespond()
+    // },1000);
+  },
 });
 
 app.mount("#root");
